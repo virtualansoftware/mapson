85fe50596c8f264e87e49be58114f4f9b5523ae4
Elan Thangamani <elans3.java@gmail.com>
2019-10-05 00:48:44 -0500

    Build Mapson

--- src/main/java/io/virtualan/mapson/Mapson.java
+++ src/main/java/io/virtualan/mapson/Mapson.java
@@ -2,24 +2,22 @@ package io.virtualan.mapson;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Mapson {
-
+    
     public static String buildMapAsJsonString(Map<String, String> jsonStructMap) {
         Map<String, Object> params = new HashMap<>();
         for (Map.Entry<String, String> mapsonEntry : jsonStructMap.entrySet()) {
             String key = mapsonEntry.getKey();
             if (key.indexOf(".") != -1) {
                 params = buildChildJson(params, key.split("\\."), mapsonEntry.getValue());
-            } else if(key.contains("[") && key.contains("]") ) {
+            } else if (key.contains("[") && key.contains("]")) {
                 String elementAt = key.substring(0, key.indexOf("["));
                 params.put(key, buildObjectList(params, key, mapsonEntry.getValue()));
-            }else {
+            } else {
                 params.put(key, mapsonEntry.getValue());
             }
         }
@@ -30,28 +28,28 @@ public class Mapson {
     private static void populateList(int index, List<Object> elementList, Object object) {
         try {
             elementList.set(index, object);
-        } catch (IndexOutOfBoundsException E){
+        } catch (IndexOutOfBoundsException E) {
             elementList.add(index, object);
         }
     }
     
     private static List<Object> extractList(String key, Map<String, Object> jsonStructMap) {
-        if(jsonStructMap.containsKey(key)) {
+        if (jsonStructMap.containsKey(key)) {
             return (List<Object>) jsonStructMap.get(key);
         } else {
-            return  new ArrayList<>();
+            return new ArrayList<>();
         }
     }
     
     
     private static List<Object> buildObjectList(Map<String, Object> jsonStructMap, String elementAt, Object value) {
         String elementAtArray = elementAt.substring(0, elementAt.indexOf("["));
-        int index = Integer.parseInt(elementAt.substring(elementAt.indexOf("[")+1, elementAt.indexOf("]")));
+        int index = Integer.parseInt(elementAt.substring(elementAt.indexOf("[") + 1, elementAt.indexOf("]")));
         List<Object> elementList = extractList(elementAtArray, jsonStructMap);
         populateList(index, elementList, value);
         return elementList;
     }
-
+    
     private static JSONObject buildJsonString(Map<String, Object> jsonStructMap) {
         JSONObject jsonObject = new JSONObject();
         for (Map.Entry<String, Object> mapEntry : jsonStructMap.entrySet()) {
@@ -69,13 +67,13 @@ public class Mapson {
             } else {
                 setObject(jsonObject, mapEntry.getKey(), mapEntry.getValue());
             }
-
+    
         }
         return jsonObject;
     }
     
-    private static void setObject(JSONObject jsonObject, String key, Object value ) {
-        if(value instanceof String) {
+    private static void setObject(JSONObject jsonObject, String key, Object value) {
+        if (value instanceof String) {
             String newValue = (String) value;
             if (newValue.indexOf("~") == 1) {
                 setObjectByType(jsonObject, key, value, newValue);
@@ -86,82 +84,82 @@ public class Mapson {
     }
     
     private static void setObjectByType(JSONObject jsonObject, String key, Object value, String newValue) {
-        String [] arrayValue = newValue.split("~");
-        if("i".equals(arrayValue[0])) {
+        String[] arrayValue = newValue.split("~");
+        if ("i".equals(arrayValue[0])) {
             jsonObject.put(key, Integer.parseInt(arrayValue[1]));
-        } else if("b".equals(arrayValue[0])) {
+        } else if ("b".equals(arrayValue[0])) {
             jsonObject.put(key, Boolean.parseBoolean(arrayValue[1]));
-        } else if("d".equals(arrayValue[0])) {
+        } else if ("d".equals(arrayValue[0])) {
             jsonObject.put(key, Double.parseDouble(arrayValue[1]));
-        } else if("l".equals(arrayValue[0])) {
+        } else if ("l".equals(arrayValue[0])) {
             jsonObject.put(key, Long.parseLong(arrayValue[1]));
-        } else if("f".equals(arrayValue[0])) {
+        } else if ("f".equals(arrayValue[0])) {
             jsonObject.put(key, Float.parseFloat(arrayValue[1]));
         } else {
             jsonObject.put(key, value);
         }
     }
     
-    private static Map<String, Object> buildChildJson(Map<String, Object> jsonStructMap, String[]  key, Object value) {
+    private static Map<String, Object> buildChildJson(Map<String, Object> jsonStructMap, String[] key, Object value) {
         String elementAt = key[0];
-        if(elementAt.contains("[") && elementAt.contains("]") ) {
+        if (elementAt.contains("[") && elementAt.contains("]")) {
             buildArrayOfObject(jsonStructMap, key, value, elementAt);
         } else {
             if (key.length == 1) {
-                jsonStructMap.put(elementAt,  value);
+                jsonStructMap.put(elementAt, value);
                 return jsonStructMap;
             }
             buildMapAsJson(jsonStructMap, key, value, elementAt);
         }
         return jsonStructMap;
     }
-
+    
     private static void buildMapAsJson(Map<String, Object> jsonStructMap, String[] key, Object value, String elementAt) {
         Map<String, Object> obj = extractDirectMap(jsonStructMap, elementAt);
         populateKeyPath(key, value, obj);
         jsonStructMap.put(elementAt, obj);
     }
-
+    
     private static void populateKeyPath(String[] key, Object value, Map<String, Object> obj) {
         String[] subKey = new String[key.length - 1];
         System.arraycopy(key, 1, subKey, 0, subKey.length);
         buildChildJson(obj, subKey, value);
     }
-
+    
     private static Map<String, Object> extractDirectMap(Map<String, Object> jsonStructMap, String elementAt) {
-        if (jsonStructMap.get(elementAt) != null  ) {
-            return  (Map<String, Object>) jsonStructMap.get(elementAt);
+        if (jsonStructMap.get(elementAt) != null) {
+            return (Map<String, Object>) jsonStructMap.get(elementAt);
         } else {
-            return  new HashMap<String, Object>();
+            return new HashMap<String, Object>();
         }
     }
-
+    
     private static void buildArrayOfObject(Map<String, Object> jsonStructMap, String[] key, Object value, String elementAt) {
         String elementAtArray = elementAt.substring(0, elementAt.indexOf("["));
-        int index = Integer.parseInt(elementAt.substring(elementAt.indexOf("[")+1, elementAt.indexOf("]")));
+        int index = Integer.parseInt(elementAt.substring(elementAt.indexOf("[") + 1, elementAt.indexOf("]")));
         List<Map<String, Object>> elementList = extractElementList(jsonStructMap, elementAtArray);
         Map<String, Object> objListMap = extractMap(index, elementList);
         populateKeyPath(key, value, objListMap);
         populateList(index, elementList, objListMap);
         jsonStructMap.put(elementAtArray, elementList);
     }
-
+    
     private static void populateList(int index, List<Map<String, Object>> elementList, Map<String, Object> objListMap) {
         try {
             elementList.set(index, objListMap);
-        } catch (IndexOutOfBoundsException E){
+        } catch (IndexOutOfBoundsException E) {
             elementList.add(index, objListMap);
         }
     }
-
+    
     private static Map<String, Object> extractMap(int index, List<Map<String, Object>> elementList) {
-        if(elementList.size() > index) {
+        if (elementList.size() > index) {
             return (Map<String, Object>) elementList.get(index);
         } else {
-            return  new HashMap<String, Object>();
+            return new HashMap<String, Object>();
         }
     }
-
+    
     private static List<Map<String, Object>> extractElementList(Map<String, Object> jsonStructMap, String elementAtArray) {
         List<Map<String, Object>> elementList;
         if (jsonStructMap.containsKey(elementAtArray)) {
@@ -175,6 +173,45 @@ public class Mapson {
             elementList = new ArrayList<>();
         }
         return elementList;
+    }
+    
+    
+    public static Map<String, String> buildMapson(String json) {
+        JSONTokener jsonTokener = new JSONTokener(json);
+        JSONObject jsonObject = new JSONObject(jsonTokener);
+        Map<String, String>  mapsonMap = new LinkedHashMap<>();
+        getJSONPath(jsonObject, null , mapsonMap);
+        return mapsonMap;
+    }
+    
+    public static void getJSONPath(Object jsonObject, String key, Map<String, String>  mapsonMap) {
+        if (jsonObject instanceof JSONObject) {
+            JSONObject jsonObject1 = (JSONObject)jsonObject;
+            Iterator<String> keys = jsonObject1.keys();
+            while (keys.hasNext()) {
+                String keey = keys.next();
+                String keeey = key == null ? keey : key +"."+ keey;
+                if (jsonObject1.optJSONArray(keey) != null) {
+                    getJSONPath(jsonObject1.optJSONArray(keey), keeey, mapsonMap);
+                } else if (jsonObject1.optJSONObject(keey) != null) {
+                    getJSONPath((JSONObject) jsonObject1.get(keey), keeey, mapsonMap);
+                } else {
+                    String prefix = "";
+                    if(jsonObject1.get(keey) instanceof Double ) {
+                        prefix = "d~";
+                    }
+                    mapsonMap.put(keeey, prefix + jsonObject1.get(keey));
+                }
+            }
+        } else  if (jsonObject instanceof JSONArray) {
+            JSONArray jsonArray = ((JSONArray) jsonObject);
+            int index = 0;
+            for (Iterator<Object> iterator = (jsonArray).iterator(); iterator.hasNext(); ) {
+                Object obj = iterator.next();
+                getJSONPath(obj, key +"["+ index++ +"]", mapsonMap);
+            }
+        }
+        return;
     }
 
 }
--- src/test/java/io/virtualan/mapson/step/MapsonStepDefinition.java
+++ src/test/java/io/virtualan/mapson/step/MapsonStepDefinition.java
@@ -1,37 +1,45 @@
 package io.virtualan.mapson.step;
 
-
-
-import static io.restassured.RestAssured.given;
-
-import java.util.Map;
-
-import com.sun.org.apache.xpath.internal.operations.Equals;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-import io.restassured.response.Response;
-import io.restassured.response.ValidatableResponse;
-import io.restassured.specification.RequestSpecification;
 import io.virtualan.mapson.Mapson;
 import org.junit.Assert;
-
+import java.util.Map;
 
 public class MapsonStepDefinition {
 
-	
-	private String PET_URL = "http://localhost:8080/api/pets";
 	private String jsonActual;
-	@Given("create a pet with given input$")
+	Map<String, String> mapson;
+	
+	@Given("Create a json with given jsonpath input$")
 	public void createPetData(Map<String, String> petMap)  {
+		mapson = petMap;
 		jsonActual = Mapson.buildMapAsJsonString(petMap);
-		System.out.println(jsonActual);
 	}
 	
 	@Then("Check the Json is Valid")
 	public void validateJson(Map<String, String> JsonExpected) {
 		Assert.assertEquals(JsonExpected.get("key"), jsonActual);
 	}
+
+	@Then("Check the reverse way mapson is Valid")
+	public void validateJson() {
+		Map<String, String> firstMap = mapson;
+		Map<String, String> secondMap = Mapson.buildMapson(jsonActual);
+		for (Map.Entry<String, String> firstEntry : firstMap.entrySet()) {
+			String firstMapValue = firstEntry.getValue();
+			String secondMapValue = secondMap.get(firstEntry.getKey());
+			if(!firstMapValue.equals(secondMapValue)){
+				System.out.println(firstEntry.getKey() + " " + firstMapValue + " " + secondMapValue + " " + firstMapValue.equals(secondMapValue));
+			}
+		}
+		Assert.assertTrue(areEqual(firstMap, secondMap));
+	}
 	
-	
+	private boolean areEqual(Map<String, String> first, Map<String, String> second) {
+		if (first.size() != second.size()) {
+			return false;
+		}
+		return first.entrySet().stream().allMatch(e -> e.getValue().equals(second.get(e.getKey())));
+	}
 }
